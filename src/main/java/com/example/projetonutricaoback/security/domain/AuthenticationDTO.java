b473d22f44364befff72ede416f1b1503a4886ce
Pedro Balen <p.balen99@gmail.com>
2025-06-23 20:49:34 -0300

    spring-security

--- src/main/java/com/example/projetonutricaoback/security/domain/AuthenticationDTO.java
+++ src/main/java/com/example/projetonutricaoback/security/domain/AuthenticationDTO.java
@@ -4,10 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthenticationDTO(
-    @NotBlank(message = "Email é obrigatório")
-    @Email(message = "Email inválido")
-    String login,
-    
-    @NotBlank(message = "Senha é obrigatória")
-    String password
+    @NotBlank @Email String login,
+    @NotBlank String password
 ) {}
--- src/main/java/com/example/projetonutricaoback/security/domain/RegisterDTO.java
+++ src/main/java/com/example/projetonutricaoback/security/domain/RegisterDTO.java
@@ -2,17 +2,11 @@ package com.example.projetonutricaoback.security.domain;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record RegisterDTO(
-    @NotBlank(message = "Nome é obrigatório")
-    String nome,
-    
-    @NotBlank(message = "Email é obrigatório")
-    @Email(message = "Email inválido")
-    String login,
-    
-    @NotBlank(message = "Senha é obrigatória")
-    String password,
-    
-    UserRole role
+    @NotBlank @Email String login,
+    @NotBlank String password,
+    @NotBlank String nome,
+    @NotNull UserRole role
 ) {}
